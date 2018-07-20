c7c3b828e6751b9189ff83faa0503132deb592b3
Hansal Shah <hnshah@linkedin.com>
2018-07-19 18:37:17 -0700

    Sanitized users.py and added various checks to make sure data is legit

--- app/views/users.py
+++ app/views/users.py
@@ -12,13 +12,30 @@ user_mod = Blueprint('user', __name__)
 @user_mod.route('/signup', methods=['POST'])
 def signup():
     logger.debug('Welcome to SIGN UP page')
+
+    # Parse the request arguments
     collection = get_collection_map('user')
-    # print collection
     json_data = json.loads(request.data)
-    username = json_data.get('username')
-    email_id = json_data.get('email')
-    # password = json_data.get('password')
-    # return jsonify(status="Bad"), 500
+    username = json_data.get('username', None)
+    email_id = json_data.get('email', None)
+    password = json_data.get('password', None)
+
+    # Check if the arguments supplied are correct. Also check the contents of the arguments
+    if username is "" or password is "" or email_id is "":
+        logger.debug("Either Username or Email-ID or Password is NULL")
+        logger.debug("Username: {0}, Email-ID: {1}, Password: {2}".format(username, email_id, password))
+        return jsonify(message="Either Username or Email-ID or Password is NULL"), 400
+
+    if username is None or password is None or email_id is None:
+        logger.debug("Username or Email-ID or Password keyword is incorrect")
+        logger.debug("Expected: username/email/password  Got: {0}".format(json_data.keys()))
+        return jsonify(message="Expected: username/email/password  Got: {0}".format(json_data.keys())), 400
+
+    if (len(json_data.keys()) != 3):
+        logger.debug("Expected: username/email/password  Got: {0}".format(json_data.keys()))
+        return jsonify(message="Incorrect number of keys. Expected: username/email/password  Got: {0}".format(json_data.keys())), 400
+
+    # Query the DB for appropriate keyword and return accordingly
     if CollectionClass(mongo.db[collection]).find_one({'username': username}) is None:
         if CollectionClass(mongo.db[collection]).insert_many([json_data]):
             return jsonify(username=username, email=email_id), 200
@@ -36,16 +53,28 @@ def signup():
 @user_mod.route('/signin', methods=['POST'])
 def signin():
     logger.debug('Welcome to SIGN IN page')
+
+    # Parse the request arguments
     collection = get_collection_map('user')
-    # print collection
     json_data = json.loads(request.data)
-    username = json_data.get('username')
-    # email_id = json_data.get('email')
-    # password = json_data.get('password')
-    # return jsonify(status="Bad"), 500
-    obj = CollectionClass(mongo.db[collection]).find_one({'$or': [{'username': username}, {'email': username}]})
-    if obj is not None:
-        if json_data.get('password') == obj['password']:
+    user = json_data.get('username', None)
+    password = json_data.get('password', None)
+
+    # Check if the arguments supplied are correct. Also check the contents of the arguments
+    if user is "" or password is "":
+        logger.debug("Either User/Email or Password is None")
+        logger.debug("User/Email: {0}, Password: {1}".format(user, password))
+        return jsonify(message="Either User/Email or Password is NULL"), 400
+
+    if user is None or password is None:
+        logger.debug("Username or Password keyword is incorrect")
+        logger.debug("Expected: username/password  Got: {0}".format(json_data.keys()))
+        return jsonify(message="Expected: username/password  Got: {0}".format(json_data.keys())), 400
+
+    # Query the DB for appropriate keyword and return accordingly
+    db_obj = CollectionClass(mongo.db[collection]).find_one({'$or': [{'username': user}, {'email': user}]})
+    if db_obj is not None:
+        if json_data.get('password') == db_obj['password']:
             logger.info('User Credentials Authentication Successful')
             return jsonify(message="User Credentials Authentication Successful"), 200
         else:
